679a623882fbc77174688c64cd49c4920ef577af
malkamat <rimka.matkalo@gmail.com>
2020-06-07 18:27:17 +0200

    ajout de la fonction ajouter au panier

--- produit.js
+++ produit.js
@@ -1,8 +1,26 @@
 const params = (new URL(document.location)).searchParams;
-const produit = params.get('produit'); 
-const id = params.get('id'); 
+const produit = params.get('produit');
+const id = params.get('id');
 const app = document.querySelector(".preview-produit")
-const test = document.querySelector(".preview-produit-description__quantite")
+const lienPanier = document.querySelector(".header__panier")
+const panier = []
+let totalPanier = 0
+
+
+ 
+for (let i = 0; i < localStorage.length;i++) {
+
+  panier.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+
+}
+
+for (let i = 0; i < panier.length; i++) {
+  totalPanier += panier[i].quantite
+}
+
+lienPanier.innerHTML = `Mon Paniere(${totalPanier})`
+
+
 
 const api = `http://localhost:3000/api/${produit}/${id}`
 
@@ -11,11 +29,11 @@ const api = `http://localhost:3000/api/${produit}/${id}`
 fetch(api)
   .then(blob => blob.json())
   .then(data => afficher(data))
-  
-    
 
-    function afficher (data) {
-const article = `        
+
+
+function afficher(data) {
+  const article = `        
 
 <img class="preview-produit__image" src="${data.imageUrl}" alt="image" class="produit__image">
     <div class="preview-produit-description">
@@ -24,7 +42,7 @@ const article = `
         <strong class="preview-produit-description__prix">${data.price/100} €</strong>
         <label for="preview-produit-description__quantite">choisir une quantité:</label>
         <select class="preview-produit-description__quantite">
-        <option value="1">1</option> 
+        <option value="1" selected>1</option> 
         <option value="2" >2</option>
         <option value="3">3</option>
         <option value="4">4</option> 
@@ -34,25 +52,44 @@ const article = `
         <option value="8" >8</option>
         <option value="9">9</option>
         <option value="10">10</option> 
-        
       </select>
+      <input class="preview-produit-description__panier" type="button" value="Ajouter la séléction au panier">
     </div>
     `
-    
-    app.innerHTML = article
-    const test = document.querySelector(".preview-produit-description__quantite")
-test.addEventListener('input', function(e) {
+
+  app.innerHTML = article
+  const panierBouton = document.querySelector(".preview-produit-description__panier")
+  const boutonQuantite = document.querySelector(".preview-produit-description__quantite")
+
+
+  boutonQuantite.addEventListener('input', function (e) {
     const prix = document.querySelector(".preview-produit-description__prix")
+    const total = (data.price / 100) * (boutonQuantite.options.selectedIndex + 1)
+    prix.innerHTML = `${total} €`
+   
 
-    let total = (data.price/100) *(test.options.selectedIndex + 1)
-    let nb = test.options.selectedIndex + 1
-        prix.innerHTML = `${total} €`
-    })
-    
-    
-  
+
+  })
+panierBouton.addEventListener("click", function (e) { 
+     const nb = boutonQuantite.options.selectedIndex + 1
+
+    class ObjetPanier {
+      constructor(id,quantite) {
+        this.id = id;
+        this.quantite = quantite;
+      }
     }
-
-
-
+    let objetPanier = new ObjetPanier(id,nb)
+     localStorage.setItem(id,JSON.stringify(objetPanier))
+  
+    window.location = "index.html"
     
+
+
+     
+
+
+ 
+  })
+     
+}
